aae10c01ca200dc0f3be868f8abc58e0a750c377
A B M Masum <abmmasum.abm@gmail.com>
2025-03-19 02:14:29 +0000

    Unique Nationalities with Sets

---
+++ unique_nationalities_with_sets.py
@@ -0,0 +1,23 @@
+nationalities = [
+    "United States",
+    "Brazil",
+    "Germany",
+    "Spain",
+    "The Netherlands",
+    "United States",
+    "United States",
+    "Australia",
+    "Japan",
+    "Egypt",
+    "South-Africa",
+    "South-Korea",
+    "China",
+    "Japan",
+    "Mexico",
+    "Germany",
+    "Sweden",
+    "The Netherlands",
+    "Canada"
+]
+
+print(set(nationalities))
